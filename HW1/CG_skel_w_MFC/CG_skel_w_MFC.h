4c68c6bfd1944c79208ca67df76f079858a09040
Yonatan <yonatan@lischinsky.com>
2024-01-23 13:24:31 +0200

    Initial commit, fixed a few BUG in skeleton code...

---
+++ HW1/CG_skel_w_MFC/CG_skel_w_MFC.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "resource.h"
+
+void display( void );
+void reshape( int width, int height );
+void keyboard( unsigned char key, int x, int y );
+void mouse(int button, int state, int x, int y);
+void fileMenu(int id);
+void mainMenu(int id);
+void initMenu();
+#ifdef CG_DEBUG
+void debug_PlayWithVectors();
+#endif
