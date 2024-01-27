81508cab8022c5f0287a4ca3d69a1ecfbd4acdfc
raysinm <raysin.maya1@gmail.com>
2024-01-27 03:18:19 +0200

    ImGui submodule works!!!

--- HW1/CG_skel_w_MFC/CG_skel_w_glfw.h
+++ HW1/CG_skel_w_MFC/CG_skel_w_glfw.h
@@ -5,21 +5,24 @@
 #include "Button.h"
 #include "resource.h"
 #include <vector>
+#include "GLFW/glfw3.h"
 
 
-
-void display(void);
-void reshape(int width, int height);
-void keyboard(unsigned char key, int x, int y);
-void mouse(int button, int state, int x, int y);
-void fileMenu(int id);
-void mainMenu(int id);
-void selectMenu(int id);
+//void display(void);
+//void reshape(int width, int height);
+//void keyboard(unsigned char key, int x, int y);
+//void mouse(int button, int state, int x, int y);
+//void fileMenu(int id);
+//void mainMenu(int id);
+//void selectMenu(int id);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 void initMenu();
 #ifdef _DEBUG
-void debug_PlayWithVectors();
-void debug_PlayWithMatrices();
+//void debug_PlayWithVectors();
+//void debug_PlayWithMatrices();
+void debug_func();
+
 #endif
 
 //----------------------------------------------------------------------------
