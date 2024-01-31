7508fc6d73d59bffca053995c1fe94c9dd46751c
raysinm <raysin.maya1@gmail.com>
2024-01-31 20:52:08 +0200

    WIP

--- HW1/CG_skel_w_MFC/MeshModel.h
+++ HW1/CG_skel_w_MFC/MeshModel.h
@@ -13,6 +13,7 @@ class MeshModel : public Model
 protected :
 	MeshModel() {}
 	vec3* vertex_positions;	// In model space- model transformations are applied immidiatly
+	int num_vertices;	//TODO
 	vec3* t_vertex_positions;	// Transformed	- Used for pipeline	- No z axis
 	vec3* vertex_normals;
 
@@ -27,14 +28,21 @@ public:
 	MeshModel(string fileName);
 	~MeshModel(void);
 	void loadFile(string fileName);
-	void MeshModel::draw(mat4& cTransform);
+	void MeshModel::draw(mat4& cTransform)
+	{
+
+
+	}
+	
 	void updateTransform()
 	{
 		mat4 trnsl_m = Translate(_trnsl.x, _trnsl.y, _trnsl.z);
-		mat4 rot_m = RotateX(_rot.x);
-		mat4 rot_m = RotateY(_rot.y);
-		mat4 rot_m = RotateZ(_rot.z);
-		mat4 trnsl_m = Translate(_trnsl.x, _trnsl.y, _trnsl.z);
+		mat4 rot_m_x = RotateX(_rot.x);
+		mat4 rot_m_y = RotateY(_rot.y);
+		mat4 rot_m_z = RotateZ(_rot.z);
+		mat4 scale_m = Scale(_scale.x, _scale.y, _scale.z);
+
+		
 
 	}
 	//void setTranslation(vec3& )
