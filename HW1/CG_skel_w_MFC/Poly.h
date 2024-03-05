ca8d9d7c5eaa16bfa3ea72d46a614e73190b61db
Yonatan <yonatan@lischinsky.com>
2024-03-05 15:20:59 +0200

    Clean Poly.h

--- HW1/CG_skel_w_MFC/Poly.h
+++ HW1/CG_skel_w_MFC/Poly.h
@@ -6,25 +6,14 @@
 
 using namespace std;
 
-typedef class Poly
+class Poly
 {
-
 	// Should contain screen coordinates only (pixel positions):
-	// 
 	// a. Should contain a set of three 'original' screen coordinates, normalized in relation to screen size even if they are outside of the screen 
 	//	  (for color calculations)
-	// 
-	// b. MAYBE (perhaps will be claculated during scanline):
-	//	  Should contain an additional vector<vec3> of actual screen coordinates- could contain a maximum
-	//	  of 6 coords in case that all triangle vertices are outside of the screen borders (and a minimum of 3
-	//	  in case all are inside the screen).
-	//	  This will be used for scanline calculations only- can be seen as "the part of the triangle that is inside
-	//	  the screen and is not necesseraly a triangle anymore"
 
 private:
 	vec3 a, b, c;
-	//vector<vec3> screen_coords;
-
 	vec3 vnA, vnB, vnC;
 	vec3 fn;
 
@@ -42,12 +31,12 @@ public:
 	//GetColor(Material& material, DrawAlgo& draw_algo);	//Will be used to calculate color?
 	~Poly(void) {};
 
-	int GetMinY() { return min_y; };
-	int GetMaxY() { return max_y; };
-	
-	int GetMinX() { return min_x; };
-	int GetMaxX() { return max_x; };
+	int GetMinY() { return min_y; }
+	int GetMaxY() { return max_y; }
+	int GetMinX() { return min_x; }
+	int GetMaxX() { return max_x; }
+
 	std::vector<Line>& GetLines() { return lines; }
 	UINT Depth(int x, int y);
 
-} Poly;
+};
